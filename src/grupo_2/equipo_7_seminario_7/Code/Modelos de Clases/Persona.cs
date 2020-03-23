8c54856ef77ab2f985415e69e066a658d327ba35
Eric <ermargar97@gmail.com>
2020-03-23 14:22:43 -0400

    Adding Code

---
+++ src/grupo_2/equipo_7_seminario_7/Code/Modelos de Clases/Persona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Modelos_de_Clases
+{
+    public class Persona
+    {
+        public Persona(string nombre)
+        {
+            Nombre = nombre;
+        }
+        public string Nombre { get; set; }
+    }
+}
---
+++ src/grupo_2/equipo_7_seminario_7/Code/Modelos de Clases/Trabajador.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Modelos_de_Clases
+{
+    public class Trabajador : Persona, ICobrarSalario
+    {
+        public Trabajador(string nombre): base(nombre)
+        {
+
+        }
+
+        public void CobrarSalario()
+        {
+            Console.WriteLine(Nombre + " cobro salario");
+        }
+    }
+}
