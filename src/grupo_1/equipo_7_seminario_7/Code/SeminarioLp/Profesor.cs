fd4b0103c66cf3f96a0ad13198c2b131f4e25bf5
JCH[LaptopLisa] <jc1997hdez@gmail.com>
2020-03-23 22:41:34 -0400

    add sem 7

---
+++ src/grupo_1/equipo_7_seminario_7/Code/SeminarioLp/Profesor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeminarioLp
+{
+    public class Profesor : Trabajador, IimpartidorClases
+    {
+
+        public Profesor(string nombre) : base(nombre)
+        { }
+        public Profesor() { }
+
+        public void ImpartirClases()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/grupo_1/equipo_7_seminario_7/Code/SeminarioLp/Trabajador.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeminarioLp
+{
+    public class Trabajador : Persona, IcobradorSalario
+    {
+        public Trabajador(string nombre) : base(nombre) { }
+        public Trabajador() { }
+        public void CobrarSalario()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
