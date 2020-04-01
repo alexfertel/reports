9d950e287ec1fa090f9199edf7eb9fa5a80ee830
Joel David <jdavidhc1710@gmail.com>
2020-03-31 21:25:03 -0700

    Ready to push

---
+++ src/grupo_2/equipo_2_seminario_11/code/IronPython/IronPython/IronPython.py
@@ -0,0 +1,77 @@
+import time
+import random
+
+# Python Quick Sort
+def partition(arr,low,high): 
+    i = (low-1)         # index of smaller element 
+    pivot = arr[high]     # pivot 
+  
+    for j in range(low , high): 
+        # If current element is smaller than or 
+        # equal to pivot 
+        if   arr[j] <= pivot: 
+            # increment index of smaller element 
+            i = i+1 
+            arr[i],arr[j] = arr[j],arr[i] 
+    arr[i+1],arr[high] = arr[high],arr[i+1] 
+    return ( i+1 )
+
+
+def quickSort(arr,low,high): 
+    if low < high: 
+        # pi is partitioning index, arr[p] is now 
+        # at right place 
+        pi = partition(arr,low,high) 
+        # Separately sort elements before 
+        # partition and after partition 
+        quickSort(arr, low, pi-1) 
+        quickSort(arr, pi+1, high)
+
+
+def bubbleSort(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            if arr[i] < arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+
+    return arr
+
+
+# Fibonacci
+def fib(a):
+    if a == 1:
+        return 0
+    elif a == 2:
+        return 1
+    else:
+        return fib(a-1) + fib(a-2)
+
+
+avg = []
+
+# BubbleSort Test
+for _ in range(30):
+    a = [random.randint(1, 1000) for i in range(10000)]
+    start = time.time()
+    bubbleSort(a)
+    end = time.time()
+    avg.append(end - start)
+
+
+# QuickSort Test
+# for _ in range(30):
+#     a = [random.randint(1, 100000) for i in range(1000000)]
+#     start = time.time()
+#     quickSort(a, 0, 99999)
+#     end = time.time()
+#     avg.append(end - start)
+
+
+# Fibonacci test
+# for _ in range(30):
+#     start = time.time()
+#     fib(35)
+#     end = time.time()
+#     avg.append(end - start)
+
+print("Operacion realizada en:", sum(avg)/len(avg))
