9fe31aa02bf26d9a7960d55078d9d139f9f2afe5
Luiso <luise98cu@gamil.com>
2020-04-07 09:33:32 -0400

    Seminario 14 Equipo 5 Python I

---
+++ src/grupo_1/equipo_5_seminario_14/Code/decorator.py
@@ -0,0 +1,74 @@
+import typing
+
+def typeCheck(fn): # Wrap Function
+    """
+    Check if the function 'fn' behaves according its annotations
+    """
+    # Gathering func info        
+    types = typing.get_type_hints(fn)
+    arg_count = fn.__code__.co_argcount
+    arg_list = fn.__code__.co_varnames
+    defaults = fn.__defaults__
+    
+    def inner_typeCheck(*args,**kwargs): # Inner Function
+        args = list(args)
+        len_args = len(args)
+        
+        # Checking kwargs for double reference
+        explicit_args = arg_list[:min(len_args,arg_count)] # Anything beyond the arg_count belongs to *args
+        for name in explicit_args:
+            if name in kwargs:
+                # Raise a TypeError
+                fn(*args,**kwargs)        
+                
+        # Inserting the defaults values
+        default_offset = len_args + len(defaults) - arg_count
+        for index in range(len_args,arg_count):
+            default_index = index - len_args + default_offset
+            default_name = arg_list[index]
+            if default_name in kwargs:
+                args.insert(arg_count-1,kwargs[default_name])
+                kwargs.__delitem__(default_name)
+            else:
+                args.insert(arg_count-1, defaults[default_index])
+        
+        # Checking arguments types
+        errors = []
+        for arg,arg_type in types.items():
+            if arg == 'return': continue
+            index = arg_list.index(arg)
+            if not isinstance(args[index],arg_type):
+                errors.append(f"Wrong Type in argument '{arg}' of function '{fn.__name__}' \nExpected {arg_type} receive {type(args[index])}")
+        
+        if not errors:
+            
+            # Calling the function
+            return_value = fn(*args,**kwargs)
+
+            # Checking return type
+            if 'return' in types:
+                if not isinstance(return_value,types['return']):
+                    errors.append(f"Wrong Type in return value of function '{fn.__name__}' \nExpected {types['return']} receive {type(return_value)}")
+                    raise TypeError(*errors)
+            
+            return return_value
+        
+        raise TypeError(*errors)
+    
+    return inner_typeCheck
+
+@typeCheck
+def fn(a:int,b=2,c:str='10',d:float='float',*args,**kwargs)->str:
+    print(a,b,c,d,args,kwargs)
+    return "7"
+
+# TypeError d:float but d is str
+# fn(1)
+
+fn(1,d=1.0)
+
+fn(1,"b type dont matter",d=1.0)
+
+fn(1,2,'3',4.0,5,6,7,e=8,n=9)
+
+fn(d=1.0,c='c',b=str,a=0,e=8,n=9)
